9c2bc31f1b9d47263291f5680f9054a8ae48f688
naoki miyagawa <ntsome21@gmail.com>
2023-02-01 15:58:25 +0000

    finndog commited sodomy just jk lol

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+let gotpubkey = false;
+let gotprivkey = false;
+
 function generateKeys() { 
     //generates random primes to use as p and q:
-    console.log('generate keys')
-    const range = [1, 100];
+    //console.log('generate keys')
+    const range = [1, 400]; //changing the range makes the numbers enormous and need to change range in the d and e variables
     const getPrimes = (min, max) => {
     const result = Array(max + 1)
     .fill(0)
@@ -41,7 +44,7 @@ function generateKeys() {
     let eval = [] //defines array for all possible values for e between 2 limits
     let dval = [] //deifines array for all possible values for d between 2 limits
 
-    for (let x= 0;x <= 100; x++) { //prints all possible candidates for e between 1 and 100: make the range bigger later
+    for (let x= 0;x <= 10000; x++) { //prints all possible candidates for e between 1 and 100: make the range bigger later
        if (1 < x < r && areCoprimes(x, r) == true) {
         eval.push(x)
        }
@@ -50,7 +53,7 @@ function generateKeys() {
     const eRandomIndex = Math.floor(Math.random() * eval.length) //gets a random index from the length of the array
     const e = eval[eRandomIndex] //selects the value from the index
 
-    for (let i= 0;i <= 10000; i++ ) { //candidates for d (the value of i must be bigger than 100 for the modulo of r to work, otherwise there are not enough candidates)
+    for (let i= 0;i <= 100000; i++ ) { //candidates for d (the value of i must be bigger than 100 for the modulo of r to work, otherwise there are not enough candidates)
         if ((i * e) % r == 1) {
             // console.log("pushing",i,"to dval")
             dval.push(i)
@@ -65,40 +68,91 @@ function generateKeys() {
     
     document.getElementById("text1").innerHTML = "value of p = " + p
     document.getElementById("text2").innerHTML = "value of q = " + q
-    document.getElementById("text3").innerHTML = "value of n = " + n + "(value of n = p*q"
-    document.getElementById("text4").innerHTML = "value of r = " + r + "(value of r = (p-1) * (q-1)"
-    document.getElementById("text5").innerHTML = "value of e = " + e + "(e must be 1 < e < r and e and r are coprime"
-    document.getElementById("text6").innerHTML = "value of d = " + d + "(d must follow (d * e) % r = 1"
-    document.getElementById("text7").innerHTML = "public key = " + publickey + "(public key = [e,n]"
-    document.getElementById("text8").innerHTML = "private key = " + privatekey + "(private key = [d,n]"
-}
-
-function checkForKeysEnc() {
-    if (gotKeys = true) {
-        encrypt()
-    } else {
-        alert("you need to generate a key pair first!")
-    }
-}
-
-function checkForKeysDec() {
-    if (gotKeys = true) {
-        decrypt()
-    } else {
-        alert("you need to have a key pair first!")
-    }
+    document.getElementById("text3").innerHTML = "value of n = " + n + " (value of n = p*q"
+    document.getElementById("text4").innerHTML = "value of r = " + r + " (value of r = (p-1) * (q-1)"
+    document.getElementById("text5").innerHTML = "value of e = " + e + " (e must be 1 < e < r and e and r are coprime"
+    document.getElementById("text6").innerHTML = "value of d = " + d + " (d must follow (d * e) % r = 1"
+    document.getElementById("text7").innerHTML = "public key = " + publickey + " (public key = [e,n]" 
+    document.getElementById("text8").innerHTML = "private key = " + privatekey + " (private key = [d,n]"
 }
 
 function encrypt() {
-   //using letter to number cipher
-   const lettersMessage = document.getElementById(messageinput).value
-   const MessageToNumbers = (a) => {
-    a = text.charCodeAt(0) //convert string of letters to numbers
-    let code = text.charCodeAt(text.length-1); 
-   }
+
+  let message = document.getElementById("message").value
+  let numberMessage = []
+  let cipher = []
+
+  for (let i = 0; i < message.length; i++) { //loops to match the number of letters in the message
+    numberMessage[i] = message.charCodeAt(i) //converts the letters to numbers one character at a time
+    cipher[i] = numberMessage[i]^e % n; // so the first ciphered letter is the first number to the power of e % n
+    console.log(cipher[i]) //outputs the cipher
+  }    
    
 }
 
 function decrypt() {
 
+  let cipher = document.getElementById("cipher").value //takes the value inside the cipher tag
+  let numbercipher = [] //this is required to convert letters to numbers before encrypting them
+  let message = []
+
+  for (let i = 0; i < cipher.length; i++) { //loops to match the length of the cipher
+    numbercipher[i] = message.charCodeAt(i) //converts the letters to numbers one character at a time
+    cipher[i] = numberMessage[i]^e % n; // so the first encrypted character is the first number to the power of e % n
+    console.log(message[i])
+  } 
+}
+
+function CheckForKeysEnc() {
+    
+}
+
+function copyPubKey() {
+    // Get the text field
+    var copyText = document.getElementById("text7");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+    //  Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+
+    gotpubkey = true
+  }
+
+function copyPrivKey() {
+       // Get the text field
+       var copyText2 = document.getElementById("text8");
+  
+       // Select the text field
+       copyText2.select();
+       copyText2.setSelectionRange(0, 99999); // For mobile devices
+     
+        // Copy the text inside the text field
+       navigator.clipboard.writeText(copyText2.value);
+     
+       // Alert the copied text
+       alert("Copied the text: " + copyText2.value);
+
+       gotprivkey = true;
+}
+
+function checkForKeysEnc() {
+  if (gotpubkey == true && gotprivkey == true) {
+    encrypt()
+  } else {
+    console.error("copy the private and public keys first by pressing the copy buttons")
+  }
+}
+
+function checkForKeysDec() {
+  if (gotpubkey == true && gotprivkey == true) {
+    decrypt()
+  } else {
+    console.error("copy the private and public keys first by pressing the copy buttons")
+  }
 }
